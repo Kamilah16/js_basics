e6f2e82fe38322c524a2aeb19cf1af857f34485c
kamilah16 <kamilah1607@gmail.com>
2025-02-21 13:59:33 +0530

    added 1

---
+++ test.js
@@ -0,0 +1,10 @@
+const accountId=134;
+let emailId="kamialh1607@gmail.com"
+var accountPassword="12345"
+let City;
+emailId="kamz.traveller@gmail.com"
+accountPassword="15"
+
+console.log(accountId,emailId,accountPassword,City);
+console.table([accountId,emailId,accountPassword,City]);
+ 
